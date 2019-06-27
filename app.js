da8119739285cd7cfa1a3e5f96df56995ab76553
Systemoutprintln <eric_solis07@hotmail.it>
2019-06-27 19:41:35 +0200

    added some comments

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ var app = express();
 var port = process.env.PORT || 8080;
 var path = require('path');
 
+
+//Controller for root
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + '/templates/index.html'));
 });
 
+//GET controller for Form
 app.get("/form", function(req, res) {
     res.sendFile(path.join(__dirname + '/templates/form.html'));
 });
 
+//POST controller for Form
 app.post("/form", function(req, res){
     console.log("i recieved a POST req");
 });
 
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log('Server running at http://localhost:'+port+'/');
 });
 
-//console.log('Server running at http://localhost:8080/');
+
