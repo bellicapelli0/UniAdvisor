6c618bdc094bf5ed92a5a320c0e378a53ba09157
Orlando Scarpa <orlando.scarpa@keypartner.com>
2019-08-27 16:21:31 +0200

    More db settings

--- src/main/java/com/uniadvisor/uniadvisor/api/Location.java
+++ src/main/java/com/uniadvisor/uniadvisor/api/Location.java
@@ -1,6 +1,10 @@
 package com.uniadvisor.uniadvisor.api;
 
+import com.uniadvisor.uniadvisor.db.Database;
+
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Map;
 
 public class Location implements Serializable {
     private final double lat;
@@ -12,7 +16,7 @@ public class Location implements Serializable {
     private final Boolean plugs;
     private final Boolean WiFi;
 
-    public Location(float lat, float lng, String longName, String shortName, String address, Boolean AC, Boolean plugs, Boolean wiFI) {
+    public Location(double lat, double lng, String longName, String shortName, String address, Boolean AC, Boolean plugs, Boolean wiFI) {
         this.lat = lat;
         this.lng = lng;
         this.longName = longName;
@@ -45,7 +49,7 @@ public class Location implements Serializable {
 
     }
 
-    public double getDist(float lo, float la){
+    public double getDist(double lo, double la){
         double lon = this.lng - lo;
         double lat = this.lat - la;
         return Math.sqrt(lon*lon + lat*lat);
@@ -55,6 +59,34 @@ public class Location implements Serializable {
         double lon = this.lng - l.lng;
         double lat = this.lat - l.lat;
         return Math.sqrt(lon*lon + lat*lat);
+    }
+
+    public static Location closest(double lat, double lng){
+        Map<String, Location> locations = Database.getLocations();
+
+        Location resultLocation = new Location();
+        double distance = 99999999;
+
+        for (Map.Entry<String, Location> entry : locations.entrySet()){
+            Location l = entry.getValue();
+            if( l.getDist(lat, lng) < distance ){
+                resultLocation = l;
+                distance = l.getDist(lat, lng);
+            }
+        }
+        return resultLocation;
+    }
+
+    public static Location[] threeClosest(double lat, double lng){
+        Map<String, Location> locations = Database.getLocations();
+        int i = 0;
+        int size = locations.size();
+        double[] arr = new double[size];
+        for(Map.Entry<String, Location> entry : locations.entrySet()){
+            arr[i++] = entry.getValue().getDist(lat,lng);
+        }
+        Arrays.sort(arr);
+        return null ;//Arrays.copyOfRange(arr,0, 2);
     }
 
     public double getLat() {
@@ -89,3 +121,4 @@ public class Location implements Serializable {
         return WiFi;
     }
 }
+
