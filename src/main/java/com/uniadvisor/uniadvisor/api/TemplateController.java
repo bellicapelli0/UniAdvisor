e28a3249bf24c7610df375194a9d9e5cc24f41b8
Systemoutprintln <eric_solis07@hotmail.it>
2019-08-28 17:51:21 +0200

    Added user name for diag template (replicable to others template). Removed unused dependency. Added static function to retrieve user informations.

--- src/main/java/com/uniadvisor/uniadvisor/api/TemplateController.java
+++ src/main/java/com/uniadvisor/uniadvisor/api/TemplateController.java
@@ -1,15 +1,27 @@
 package com.uniadvisor.uniadvisor.api;
 
+import com.uniadvisor.uniadvisor.util.AuthorizedClientServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
 
 @Controller
 public class TemplateController {
 
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
 
     @GetMapping("{location}")
-    public String locations(@PathVariable("location") String location){
+    public String locations(@PathVariable("location") String location, Model model, OAuth2AuthenticationToken authentication){
+        Map userAttributes = AuthorizedClientServiceUtil.getAttributes(authorizedClientService, authentication);
+        model.addAttribute("name", userAttributes.get("name"));
         return location;
     }
 
---
+++ src/main/java/com/uniadvisor/uniadvisor/util/AuthorizedClientServiceUtil.java
@@ -0,0 +1,42 @@
+package com.uniadvisor.uniadvisor.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+public class AuthorizedClientServiceUtil {
+
+
+    public static Map getAttributes(OAuth2AuthorizedClientService authorizedClientService, OAuth2AuthenticationToken authentication) {
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+
+        String userInfoEndpointUri = client.getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUri();
+
+        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+                    .getTokenValue());
+
+            HttpEntity<String> entity = new HttpEntity<String>("", headers);
+
+            ResponseEntity<Map> response = restTemplate.exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+            Map userAttributes = response.getBody();
+            return userAttributes;
+//              model.addAttribute("test", userAttributes.get"")
+        }
+        return null;
+    }
+}
