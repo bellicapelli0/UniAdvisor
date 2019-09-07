e9095a214f6afed1784cb0e7becf4f0f4143dfa7
Systemoutprintln <eric_solis07@hotmail.it>
2019-09-07 18:02:12 +0200

    Added new data for InitDB

--- src/main/java/com/uniadvisor/uniadvisor/api/InitDB.java
+++ src/main/java/com/uniadvisor/uniadvisor/api/InitDB.java
@@ -21,8 +21,8 @@ public class InitDB {
         Map<String, Location> locations = Database.getLocations();
         locations.put("diag", new Location());
 
-        locations.put("exposte", new Location(41.896335, 12.5177278, "EX Poste - Edificio Marco Polo - Sapeienza Università di Roma", "exposte", "Via dello Scalo San Lorenzo, 82", Boolean.TRUE, Boolean.FALSE, Boolean.TRUE));
-        locations.put("lab", new Location(41.8998813, 12.516822, "Laboratorio di Informatica Paolo Ercoli", "lab", "Via Tiburtina, 205", Boolean.TRUE, Boolean.TRUE, Boolean.TRUE));
+//        locations.put("exposte", new Location(41.896335, 12.5177278, "EX Poste - Edificio Marco Polo - Sapeienza Università di Roma", "exposte", "Via dello Scalo San Lorenzo, 82", Boolean.TRUE, Boolean.FALSE, Boolean.TRUE));
+//        locations.put("lab", new Location(41.8998813, 12.516822, "Laboratorio di Informatica Paolo Ercoli", "lab", "Via Tiburtina, 205", Boolean.TRUE, Boolean.TRUE, Boolean.TRUE));
 
         locations.put("exposte", new Location(41.896, 12.52, "EX Poste - Edificio Marco Polo - Sapeienza Università di Roma", "exposte", "Via dello Scalo San Lorenzo, 82", Boolean.TRUE, Boolean.FALSE, Boolean.TRUE));
         locations.put("lab", new Location(41.899, 12.516, "Laboratorio di Informatica Paolo Ercoli", "lab", "Via Tiburtina, 205", Boolean.TRUE, Boolean.TRUE, Boolean.TRUE));
@@ -41,6 +41,63 @@ public class InitDB {
         diag.add(4);
         diag.add(5);
         stars.put("diag", diag);
+
+        LinkedList<Integer> lab = new LinkedList<>();
+        lab.add(2);
+        lab.add(2);
+        lab.add(5);
+        stars.put("lab", lab);
+
+        LinkedList<Integer> exposte = new LinkedList<>();
+        exposte.add(4);
+        exposte.add(5);
+        stars.put("exposte", exposte);
+
+        LinkedList<Integer> pratone = new LinkedList<>();
+        pratone.add(2);
+        pratone.add(2);
+        pratone.add(4);
+        stars.put("pratone", pratone);
+
+        LinkedList<Integer> mirafiori = new LinkedList<>();
+        mirafiori.add(4);
+        mirafiori.add(4);
+        stars.put("mirafiori", mirafiori);
+
+        LinkedList<Integer> spv = new LinkedList<>();
+        spv.add(3);
+        spv.add(4);
+        spv.add(5);
+        spv.add(5);
+        stars.put("spv", spv);
+
+        LinkedList<Integer> geologia = new LinkedList<>();
+        geologia.add(3);
+        geologia.add(4);
+        geologia.add(4);
+        geologia.add(4);
+        geologia.add(5);
+        stars.put("geologia", geologia);
+
+        LinkedList<Integer> maxxi = new LinkedList<>();
+        maxxi.add(3);
+        maxxi.add(5);
+        stars.put("maxxi", maxxi);
+
+        LinkedList<Integer> roma3 = new LinkedList<>();
+        roma3.add(3);
+        roma3.add(3);
+        roma3.add(3);
+        roma3.add(3);
+        roma3.add(3);
+        stars.put("roma3", roma3);
+
+        LinkedList<Integer> bellearti = new LinkedList<>();
+        bellearti.add(4);
+        bellearti.add(4);
+        bellearti.add(5);
+        stars.put("bellearti", bellearti);
+
 
         Database.commit();
 
