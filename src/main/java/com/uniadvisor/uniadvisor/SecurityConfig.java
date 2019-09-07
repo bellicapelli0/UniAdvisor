0b00b7196853067460e3c17c849bec0d5847aabc
Systemoutprintln <eric_solis07@hotmail.it>
2019-09-07 17:23:00 +0200

    Added support for new home

--- src/main/java/com/uniadvisor/uniadvisor/SecurityConfig.java
+++ src/main/java/com/uniadvisor/uniadvisor/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll()
+        http.authorizeRequests().antMatchers("/", "img/logo.png").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
